bc198113b530c58a42581bec16b3de9725adeb44
WAAutoMaton <a547673135@gmail.com>
2022-10-26 20:53:36 +0800

    Revert "feat: priority-sema"
    This reverts commit b87039f31c589a8716b9276d58ed5da5b24f8162.

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -113,14 +113,10 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) {
-    struct thread *t = thread_list_pop_max_priority(&sema->waiters);
-    thread_unblock(t);
-  }
+  if (!list_empty (&sema->waiters)) 
+    thread_unblock (list_entry (list_pop_front (&sema->waiters),
+                                struct thread, elem));
   sema->value++;
-  if (old_level == INTR_ON) {
-    thread_yield();
-  }
   intr_set_level (old_level);
 }
 
